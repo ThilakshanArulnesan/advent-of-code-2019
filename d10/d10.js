f51709d53d0dee3b80347b4a5b74415067ad94cc
Thilakshan <th_thilakshan@hotmail.com>
2019-12-10 12:09:50 -0500

    add d10 problem 2 (part way, mistake somewhere

--- d10/d10.js
+++ d10/d10.js
@@ -24,46 +24,48 @@ readFile(`10.txt`, '\n')
         }
       }
     }
-    // console.log(matrix);
-    // console.log(asteroidLocations);
 
     let asteroidCount = asteroidLocations.map((asteroid, i, asteroidLocations) => {
-      let angles = new Set(); //store the slopes found so far (round to 4 dec. to avoid decimal approx errors)
+      let slopes = {}; //store the slopes found so far (round to 4 dec. to avoid decimal approx errors)
       let numAsteroids = 0;
       for (let otherAsteroid of asteroidLocations) {
         if (otherAsteroid === asteroid) continue;
-
-        let dy = -(otherAsteroid.y - asteroid.y); //Up is down and down is up
+        //Note slopes of infinity are allowed (vertical line)
+        let dy = otherAsteroid.y - asteroid.y;
         let dx = otherAsteroid.x - asteroid.x
 
-        let angle = Math.acos(dy / Math.sqrt(dx ** 2 + dy ** 2)); //Four decimals (arbritrary, would need more as size of puzzle increases)
-        if (dx < 0) {
-          if (dy < 0) {
-            angle = angle + Math.PI / 2;
-          } else if (dy > 0) {
-            angle = angle + 3 * Math.PI / 2;
-          } else if (dy === 0) {
-            angle = angle + Math.PI;
-          }
-        }
-        if (isNaN(angle)) {
+        let slope = Math.round(dy / dx * 10000) / 10000; //Four decimals (arbritrary, would need more as size of puzzle increases)
+
+        if (slopes[slope] === undefined) {
           if (dy > 0) {
-            angle = Math.PI / 2;
-          } else {
-            angle = 3 * Math.PI / 2
+            slopes[slope] = [true, false];
+          } else if (dy < 0) {
+            slopes[slope] = [false, true];
+          } else {//dy = 0
+            if (dx > 0) {
+              slopes[slope] = [true, false];
+            } else {
+              slopes[slope] = [false, true];
+            }
           }
-        }
-
-        angle = Math.round(angle * 10000) / 10000;
-        // console.log(angle);
-        if (asteroid.x === 2 && asteroid.y === 2) {
-          console.log(`Asteroid ${otherAsteroid.x},${otherAsteroid.y} has an angle of: ${angle}. dx=${dx}, dy=${dy}`);
-        }
-
-        if (!angles.has(angle)) {
-          angles.add(angle);
           numAsteroids++;
         }
+        else if (dy > 0 && !slopes[slope][0]) {
+          slopes[slope] = [true, true];
+          numAsteroids++;
+        } else if (dy < 0 && !slopes[slope][1]) {
+          slopes[slope] = [true, true];
+          numAsteroids++;
+        } else if (dy === 0) {
+          if (dx > 0 && !slopes[slope][0]) {
+            slopes[slope] = [true, true];
+            numAsteroids++;
+          } else if (dx < 0 && !slopes[slope][1]) {
+            slopes[slope] = [true, true];
+            numAsteroids++;
+          }
+        }
+
 
       }
 
@@ -72,4 +74,86 @@ readFile(`10.txt`, '\n')
     console.log(asteroidCount);
     let bestAsteroid = asteroidCount.reduce((p, c) => c.num > p.num ? c : p, asteroidCount[0]);
     console.log(bestAsteroid);
-  }); 
+  });
+
+/*Solution using angles instead
+
+const fs = require('fs');
+
+const readFile = (filename, splitToken) => {
+return new Promise((res, rej) => {
+  fs.readFile(`./${filename}`, 'utf8', (err, data) => {
+    if (err) throw err;
+    res(data.trim().split(splitToken));
+  });
+})
+};
+
+
+readFile(`10.txt`, '\n')
+.then(res => {
+  let asteroidLocations = [];
+  let matrix = [];
+  for (let [i, val] of res.entries()) {
+    matrix[i] = [];
+    for (let j in val) {
+      let letter = val[j];
+      matrix[i].push(letter);
+      if (letter === '#') {
+        asteroidLocations.push({ x: Number(j), y: i })
+      }
+    }
+  }
+
+  let asteroidCount = asteroidLocations.map((asteroid, i, asteroidLocations) => {
+    let angles = new Set(); //store the slopes found so far (round to 4 dec. to avoid decimal approx errors)
+    let numAsteroids = 0;
+    for (let otherAsteroid of asteroidLocations) {
+      if (otherAsteroid === asteroid) continue;
+
+      let dy = -(otherAsteroid.y - asteroid.y); //Up is down and down is up
+      let dx = otherAsteroid.x - asteroid.x
+
+      let angle = Math.acos(dy / Math.sqrt(dx ** 2 + dy ** 2)); //Four decimals (arbritrary, would need more as size of puzzle increases)
+      if (dx < 0) { // Corrects based on the quadrant since acos(x) maps angles >180 to angles < 180
+        if (dy < 0) {
+          angle = angle + Math.PI / 2;
+        } else if (dy > 0) {
+          angle = angle + 3 * Math.PI / 2;
+        } else if (dy === 0) {
+          angle = angle + Math.PI;
+        }
+      }
+
+      if (isNaN(angle)) { //Angle is 90 or 270
+        if (dy > 0) {
+          angle = Math.PI / 2;
+        } else {
+          angle = 3 * Math.PI / 2
+        }
+      }
+
+      angle = Math.round(angle * 10000) / 10000;
+      // console.log(angle);
+      if (asteroid.x === 2 && asteroid.y === 2) {
+        console.log(`Asteroid ${otherAsteroid.x},${otherAsteroid.y} has an angle of: ${angle}. dx=${dx}, dy=${dy}`);
+      }
+
+      if (!angles.has(angle)) {
+        angles.add(angle);
+        numAsteroids++;
+      }
+
+    }
+
+    return { loc: asteroid, num: numAsteroids };
+  });
+  console.log(asteroidCount);
+  let bestAsteroid = asteroidCount.reduce((p, c) => c.num > p.num ? c : p, asteroidCount[0]);
+  console.log(bestAsteroid);
+});
+
+
+
+
+*/
--- d10/d10_v2.js
+++ d10/d10_v2.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-
+const chalk = require('chalk');
 const readFile = (filename, splitToken) => {
   return new Promise((res, rej) => {
     fs.readFile(`./${filename}`, 'utf8', (err, data) => {
@@ -10,7 +10,7 @@ const readFile = (filename, splitToken) => {
 };
 
 
-readFile(`10.txt`, '\n')
+readFile(`ex.txt`, '\n')
   .then(res => {
     let asteroidLocations = [];
     let matrix = [];
@@ -24,77 +24,94 @@ readFile(`10.txt`, '\n')
         }
       }
     }
-    // console.log(matrix);
-    // console.log(asteroidLocations);
-
-    let asteroidCount = asteroidLocations.map((asteroid, i, asteroidLocations) => {
-      let slopes = {}; //store the slopes found so far (round to 4 dec. to avoid decimal approx errors)
+    mapping = {};
+    bestMapping = {};
+    bestAsteroid = {};
+    let maxSoFar = 0;
+    asteroidLocations.forEach((asteroid, i, asteroidLocations) => {
+      let angles = new Set(); //store the slopes found so far (round to 4 dec. to avoid decimal approx errors)
       let numAsteroids = 0;
+
       for (let otherAsteroid of asteroidLocations) {
         if (otherAsteroid === asteroid) continue;
-        //Note slopes of infinity are allowed (vertical line)
-        let dy = otherAsteroid.y - asteroid.y;
+
+        let dy = -(otherAsteroid.y - asteroid.y); //Up is down and down is up
         let dx = otherAsteroid.x - asteroid.x
 
-        let slope = Math.round(dy / dx * 10000) / 10000; //Four decimals (arbritrary, would need more as size of puzzle increases)
-        // if (asteroid.x === 2 && asteroid.y === 2) {
-        //   console.log(`Asteroid ${otherAsteroid.x},${otherAsteroid.y} has a slope of: ${slope}.`);
-        // }
+        let angle = Math.acos(dy / Math.sqrt(dx ** 2 + dy ** 2)); //Four decimals (arbritrary, would need more as size of puzzle increases)
+        if (dx < 0) { // Corrects based on the quadrant since acos(x) maps angles >180 to angles < 180
+          if (dy < 0) {
+            angle = angle + Math.PI / 2;
+          } else if (dy > 0) {
+            angle = angle + 3 * Math.PI / 2;
+          } else if (dy === 0) {
+            angle = angle + Math.PI;
+          }
+        }
 
-        if (slopes[slope] === undefined) {
-          // if (asteroid.x === 2 && asteroid.y === 2) {
-          //   console.log(`   Adding asteroid ${otherAsteroid.x},${otherAsteroid.y} (first time slope).`);
-          // }
+        if (isNaN(angle)) { //Angle is 90 or 270
           if (dy > 0) {
-            slopes[slope] = [true, false];
-          } else if (dy < 0) {
-            slopes[slope] = [false, true];
-          } else { //dy = 0
-            // if (asteroid.x === 2 && asteroid.y === 2) {
-            //   console.log(`   In the else.`);
-            // }
-            if (dx > 0) {
-              slopes[slope] = [true, false];
-              // if (asteroid.x === 2 && asteroid.y === 2) {
-              //   console.log(`   dx>0.`);
-              // }
-            } else {
-              slopes[slope] = [false, true];
-              // if (asteroid.x === 2 && asteroid.y === 2) {
-              //   console.log(`   dx<0.`);
-              // }
-            }
-          }
-          numAsteroids++;
-        }
-        else if (dy > 0 && !slopes[slope][0]) {
-          slopes[slope] = [true, true];
-          numAsteroids++;
-        } else if (dy < 0 && !slopes[slope][1]) {
-          slopes[slope] = [true, true];
-          numAsteroids++;
-        } else if (dy === 0) {
-          if (dx > 0 && !slopes[slope][0]) {
-            slopes[slope] = [true, true];
-            numAsteroids++;
-            // if (asteroid.x === 2 && asteroid.y === 2) {
-            //   console.log(`   Adding asteroid ${otherAsteroid.x},${otherAsteroid.y} (dx>0 first time).`);
-            // }
-          } else if (dx < 0 && !slopes[slope][1]) {
-            slopes[slope] = [true, true];
-            numAsteroids++;
-            // if (asteroid.x === 2 && asteroid.y === 2) {
-            //   console.log(`   Adding asteroid ${otherAsteroid.x},${otherAsteroid.y} (dx< 0 first time).`);
-            // }
+            angle = Math.PI / 2;
+          } else {
+            angle = 3 * Math.PI / 2
           }
         }
 
+        angle = Math.round(angle * 1000) / 1000;
+        if (!mapping[angle]) {
+          mapping[angle] = [];
+          mapping[angle].push(otherAsteroid);
+        } else {
+          mapping[angle].push(otherAsteroid);
+        }
+        // console.log(angle);
+        if (asteroid.x === 2 && asteroid.y === 2) {
+          console.log(`Asteroid ${otherAsteroid.x},${otherAsteroid.y} has an angle of: ${angle}. dx=${dx}, dy=${dy}`);
+        }
+
+        if (!angles.has(angle)) {
+          angles.add(angle);
+          numAsteroids++;
+        }
 
       }
+      if (numAsteroids >= maxSoFar) {
+        console.log(`Best is at ${asteroid.x},${asteroid.y}`)
+        maxSoFar = numAsteroids;
+        bestAsteroid = asteroid;
+        bestMapping = { ...mapping };
+      }
 
-      return { loc: asteroid, num: numAsteroids };
+      mapping = {}; //reset
+      // return { loc: asteroid, num: numAsteroids };
     });
-    console.log(asteroidCount);
-    let bestAsteroid = asteroidCount.reduce((p, c) => c.num > p.num ? c : p, asteroidCount[0]);
-    console.log(bestAsteroid);
+    let sortedKeys = Object.keys(bestMapping).sort((a, b) => a - b);
+    console.log(sortedKeys);
+    let count = 0;
+    while (count < 1) {
+      for (let angle of sortedKeys) {
+        console.log(angle, bestMapping[angle].map((ast) => {
+          return { x: ast.x - bestAsteroid.x, y: ast.y - bestAsteroid.y }
+        }));
+        if (bestMapping[angle].length > 0) {
+          count++;
+          let closest = (bestMapping[angle][0].x - bestAsteroid.x) ** 2 + (bestMapping[angle][0].y - bestAsteroid.y) ** 2;
+
+          let poppedAsteroid = bestMapping[angle].reduce((prevBest, current) => {
+            if ((current.x - bestAsteroid.x) ** 2 + (current.y - bestAsteroid.y) ** 2 <= closest) {
+              return current;
+            } else {
+              return prevBest;
+            }
+          }, bestMapping[angle][0]);
+
+          bestMapping[angle] = bestMapping[angle].filter(asteroid => asteroid !== poppedAsteroid);//pew pew
+
+          console.log(`The ${chalk.blue(count)} asteroid to be vaporized is at ${poppedAsteroid.x}, ${poppedAsteroid.y} (${poppedAsteroid.x - bestAsteroid.x},${poppedAsteroid.y - bestAsteroid.y})`)
+          if (count === 200) {
+            console.log(poppedAsteroid);
+          }
+        }
+      }
+    }
   }); 
