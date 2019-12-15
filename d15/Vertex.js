736e216d0611fe2852143cd282a421529432e7b7
Thilakshan <th_thilakshan@hotmail.com>
2019-12-15 12:02:21 -0500

    add d15 solutions. Required an exploration of the maze (simple always turn right algorithm, followed by djikstras algorithm to find min distance

---
+++ d15/Vertex.js
@@ -0,0 +1,37 @@
+class Vertex {
+
+  constructor(pos, north, east, south, west) {
+    this.pos = pos;
+    this.neighbours = [north, south, west, east];
+    this.dist = Infinity;
+  }
+
+  addNeighbour(neighbour, dir = 1) {
+    //links neigbors together.
+    this.neighbours[dir - 1] = neighbour;
+
+
+    if (dir === 1) {
+      dir = 2;
+    } else if (dir === 2) {
+      dir = 1;
+    } else if (dir === 3) {
+      dir = 4
+    } else if (dir === 4) {
+      dir = 3;
+    }
+
+    neighbour.neighbours[dir - 1] = this;
+  }
+
+  addWall(dir = 1) {
+    this.neighbours[dir - 1] = null;
+  }
+
+  identifier() {
+    return `${this.pos.x},${this.pos.y}`;
+  }
+
+}
+
+module.exports = { Vertex };
