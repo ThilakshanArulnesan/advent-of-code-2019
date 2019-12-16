d0ca461a6f6272d750a53a6b0d8f6d3637824b66
Thilakshan <th_thilakshan@hotmail.com>
2019-12-16 12:34:18 -0500

    add comments for explaining solution

--- d16/d16_v2.js
+++ d16/d16_v2.js
@@ -1,3 +1,6 @@
+/*
+Note the solution to d16 is only solvable quickly if the message offset > 75% of the input length. This will then ensure that all numbers before the offset are multiplied by 0, and all numbers after (and the number itself) are multiplied by 1. Without this assumption the problem takes much longer to compute.
+*/
 const fs = require('fs');
 
 const readFile = (filename, splitToken) => {
@@ -10,15 +13,17 @@ const readFile = (filename, splitToken) => {
 };
 
 //Applies fft as described in problem
-//[ 2, 4, 1, 7, 6, 1, 7, 6 ]
 const fft = (input, msgOffset = 0) => {
   let out = new Array(msgOffset).fill(0);
   let appendArr = [];
 
-  let prev = 0;
-  for (let i = input.length - 1; i >= msgOffset; i--) {
+  let prev = 0; //prev stores the value of the last digit, which is the sum of all the digits after it (and including it) in the previous iteration. We can use this value to calculate the next term (working from the end of the array backward)
+
+  //EX [... 1 2 3 4] => [... x x x 4] => [... x x 7 4] => [... x 9 7 4] => [... 0 9 7 4]
+  //      i = N-1    =>     i=N-2     =>     i=N-3     =>      i=N-4    =>  i=N-5
+  for (let i = input.length - 1; i >= msgOffset; i--) { //Loop backwards
     prev += input[i];
-    appendArr.push(prev % 10);
+    appendArr.push(prev % 10); //Remainder 10 to get the last digit. Always positive b/c initial input digits are always positive
   }
   appendArr = appendArr.reverse();
   appendArr.forEach(v => out.push(v));
